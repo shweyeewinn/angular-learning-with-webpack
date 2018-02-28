e96e1fc1ac5598550066d92e2ba62d34a4ca76d3
shwe yee <yonngelay@gmail.com>
2018-02-28 09:27:31 +0800

    first commit of public and src folder

---
+++ src/app/admin/adminShared/user.service.ts
@@ -0,0 +1,75 @@
+import { Injectable } from '@angular/core';
+import {  
+    CanActivate, 
+    Router,
+    ActivatedRouteSnapshot,
+    RouterStateSnapshot
+} from '@angular/router';
+import * as firebase from 'firebase';
+
+
+@Injectable()
+export class UserService implements CanActivate {
+    userLoggedIn: boolean = false;
+    loggedInUser: string;
+    authUser: any;
+    
+    constructor( private router: Router ) {
+        firebase.initializeApp({
+            apiKey: "AIzaSyC_kQQWDK22CMq9y4U90KGmGY5Ej29bXQ8",
+            authDomain: "gigabytegames-69d2f.firebaseapp.com",
+            databaseURL: "https://gigabytegames-69d2f.firebaseio.com",
+            projectId: "gigabytegames-69d2f",
+            storageBucket: "gigabytegames-69d2f.appspot.com",
+            messagingSenderId: "617787799469"
+        })
+     }
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean { 
+        let url: string = state.url;
+        return this.verifyLogin(url);
+    }   
+
+    verifyLogin(url: string): boolean {
+        if (this.userLoggedIn) { return true; }
+                
+        this.router.navigate(['/admin/login']);
+        return false;
+    }
+
+    register(email: string, password: string){
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .catch(function(error) {
+                alert(`${error.message} Please Try Again!`);
+        });
+    }
+
+    verifyUser() {
+        this.authUser = firebase.auth().currentUser;
+        if (this.authUser) {
+
+            alert(`Welcome ${this.authUser.email}`);
+            this.loggedInUser = this.authUser.email;
+            this.userLoggedIn = true;
+            this.router.navigate(['/admin']);
+        }
+    }
+
+    login(loginEmail: string, loginPassword: string) {
+        firebase.auth().signInWithEmailAndPassword(loginEmail, loginPassword)
+            .catch(function(error) {
+                alert(`${error.message} Unable to login. Try again!`);
+        });
+    }
+
+    logout(){
+        this.userLoggedIn = false;
+        firebase.auth().signOut().then(function() {
+            alert(`Logged Out!`);
+
+        }, function(error) {
+            alert(`${error.message} Unable to logout. Try again!`);
+        });
+    }
+
+}
